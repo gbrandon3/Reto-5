31c2e49a5fcf1244c9a92d06d4355e04550c62dc
gbrandon3 <b.gonzalezp@uniandes.edu.co>
2023-09-12 20:09:43 -0500

    Ajuste variables

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -166,7 +166,7 @@ MQTT_PORT = 8082
 
 # Credenciales para el usuario suscriptor del bróker MQTT
 MQTT_USER = "admin"
-MQTT_PASSWORD = "admin1234"
+MQTT_PASSWORD = "admin"
 
 # Credenciales para el usuario publicador del bróker MQTT
 MQTT_USER_PUB = "admin2"
