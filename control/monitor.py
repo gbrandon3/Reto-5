59df1e0a43d1eafc999338134c14a7c394fb66c2
gbrandon3 <b.gonzalezp@uniandes.edu.co>
2023-09-13 20:25:27 -0500

    Monitor

--- control/monitor.py
+++ control/monitor.py
@@ -50,9 +50,9 @@ def analyze_data2():
         user = item['station__user__username']
         valor_medio = (max_value + min_value) / 2.0
         
-        if item["check_value"] != valor_medio and item["check_value"] > max_value or item["check_value"] < min_value:
+        if item["check_value"] != valor_medio and (item["check_value"] <= max_value and item["check_value"] >= min_value):
             resultado="Baja"if item['check_value']<valor_medio else"Alta"
-            message=message+"{} promedio:{},".format(variable,resultado)
+            message=message+"{} promedio {},".format(variable,resultado)
             alert = True
             alerts += 1
 
